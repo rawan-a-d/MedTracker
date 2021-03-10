750588818987f22b2722c9c8e8e3c2e9dac00921
Alayoubi,Ranim R <r.alayoubi@student.fontys.nl>
2021-03-10 23:12:57 +0100

    NavigationBottomBar is created:
    Navigation between home page and my medicines is set

--- app/src/main/java/com/medtracker/MyMedicinesActivity.kt
+++ app/src/main/java/com/medtracker/MyMedicinesActivity.kt
@@ -5,29 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MyMedicinesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_my_medicines)
 
         //intialize and assign functionalities
         val bottomNavigator = findViewById<BottomNavigationView>(R.id.bottom_navigator)
 
-        //set the selected icon on home page
-//        bottomNavigator.setSelectedItemId(R.id.home)
-        bottomNavigator.selectedItemId = R.id.home
+        //set the selected icon on mymedicines page
+//        bottomNavigator.setSelectedItemId(R.id.MyMedicines)
+        bottomNavigator.selectedItemId = R.id.myMedicines
 
         //navigation functionalities
         bottomNavigator.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.home -> {
-//                    val intent = Intent(this, MainActivity::class.java)
-//                    startActivity(intent)
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
                     true
                 }
                 R.id.myMedicines -> {
-                    val intent = Intent(this, MyMedicinesActivity::class.java)
-                    startActivity(intent)
+//                    val intent = Intent(this, MyMedicinesActivity::class.java)
+//                    startActivity(intent)
                     true
                 }
                 //for the switch profile tab
