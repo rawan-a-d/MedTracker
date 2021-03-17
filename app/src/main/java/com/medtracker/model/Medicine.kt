4d22eadcd3871f25292cf7311ce42c09ca11e9e4
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2021-03-17 06:28:40 +0100

    Readd Medicine

---
+++ app/src/main/java/com/medtracker/model/Medicine.kt
@@ -0,0 +1,17 @@
+package com.medtracker.model
+
+data class Medicine (
+    val id: String,
+    val name: String,
+    val description: String,
+    val usage: String,
+    val side_effects: String,
+) {
+    constructor() : this("", "", "", "", "") {
+
+    }
+
+    override fun toString(): String {
+        return "Medicine(id='$id', name='$name', description='$description', usage='$usage', side_effects='$side_effects')"
+    }
+}
