6177d62e64770a1571ad935f89c5439370f07fa6
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2021-03-17 06:04:38 +0100

    Restore medicine class

--- app/src/main/java/com/medtracker/model/medicine.kt
+++ app/src/main/java/com/medtracker/model/medicine.kt
@@ -1,19 +1,8 @@
 package com.medtracker.model
 
 data class Medicine (
-    val id: String,
-    val name: String,
-    val description: String,
-    val usage: String,
-    val side_effects: String,
+    val id: String? = null,
+    val name: String? = null,
 ){
-    constructor(): this("", "","","","") {
-
-    }
-
-    override fun toString(): String {
-        return "Medicine(id='$id', name='$name', description='$description', usage='$usage', side_effects='$side_effects')"
-    }
-
 
 }
